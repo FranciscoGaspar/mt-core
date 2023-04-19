058235d8936030bc1a59fbd829a11e88b0cf1871
Francisco Gaspar <f.gaspar@kigroup.de>
2023-04-19 14:49:24 +0100

    feat(MT-06): add global prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppService } from './app.service';
 import { UserSchema } from './user/schema/user.schema';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', { dbName: 'nest' }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [AppService, UserService],
+  providers: [UserService],
 })
 export class AppModule {}
