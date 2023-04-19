befb8910674f4a8bb83c137db754dda79e635c6a
Francisco Gaspar <f.gaspar@kigroup.de>
2023-04-19 12:29:53 +0100

    feat(MT-06): add user dto, schema and interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { UserSchema } from './user/schema/user.schema';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
